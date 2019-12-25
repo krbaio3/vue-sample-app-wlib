01219e4a2ffd8263e04d0a2f74052d2aefb3b40e
krbaio3 <krbaio3@gmail.com>
2019-12-25 13:12:13 +0100

    vue-sample-app-wlib

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+## Generate Lib
+https://itnext.io/create-a-vue-js-component-library-as-a-module-part-1-a1116e632751
+https://cli.vuejs.org/guide/build-targets.html#using-vuex-in-builds
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DummyButton/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import DummyButton from 'dummyButton';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
+    DummyButton,
   },
 };
 </script>
